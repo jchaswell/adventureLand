85757f3f724c9df245b3be35a20b0ef4734ca8af
jchaswell <jchaswell@gmail.com>
2020-02-21 18:05:41 +0000

    added - 99bottles - performance trick - auto accept party invite - on_combined_damage

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ Ideas / Todo
   - Party Leader dont attack if any party member hp low
   - anchor movement to party leader position
 - use upgrade function automagically
+  - item_grade(item) // example: item_grade(character.items[0])
+  - item_properties(item) // example: item_properties(character.items[0])
 - don't use potions unless below certain threshold
 - use skills/mana
 - auto respawn
 - auto reconnect
 - resume farming
+- visit town
+- refil potions
+- make ranged vs melee attackmodes (ranged kites)
+- handle on_combined_damage()
 */
 
 /* *******Settings******** */
@@ -18,29 +24,33 @@ var monsterReqStats = {
 	min_xp:100,
 	max_att:100
 }
-var percentHpRequiredToAcquireNewTarget = 0.5; //can't be const for some reason
-var maxWander = 10;
+var percentHpRequiredToAcquireNewTarget = 0.5; //can't be a const for some reason
+var maxWander = 50; //max movement allowed after clicking Engage
+var autoAcceptPartyInviteFrom = ['JCaesar', 'JCameron', 'JCarrey', 'BobDonut', 'CardiB'];
 /*****************************/
+
 
 
 setInterval(function(){
   if(character.rip){
-    return; 
+    attack_mode = false;
+	  respawn();
+	  return; 
   }
   use_hp_or_mp(); 
   loot();
   if(attack_mode && !is_moving(character)){ 
     attackMode();
   }
-},1000/2);
+},1000/4);
 
 function attackMode(){
   //sets 'target' variable to whatever is already targetted
   var target=get_targeted_monster(); 
   
-	
-  //if we dont have a target already
   if (!target){
+    //We don't have a current target.  Don't get a new one if
+    //low on HP, or as they say in England, HAYTCH P.
     if (hasEnoughHPForNewTarget()){
 	  
 		  target = get_nearest_monster(monsterReqStats);
@@ -49,32 +59,29 @@ function attackMode(){
       	set_message('no monsters');
       	return;
       }
-    } else {
-      set_message('Hold 4 HP');
-      return;
-    }
-  //If we get here, we have a target. Now, if it isn't in range, decide what to do
-  } else {
-	  game_log(target.name);
-    //This is primarily for melee characters. If they are targeting
-    //something that isn't 
-    //reachable, try switching to closer target. But makes sure current 
-    //target isn't already aggroing character as safety check so we 
-    //dont aggro multiple monsters
-    if (!targetIsWithinRestrictedRange(target) && target.target !== character.name){	
-      target = get_nearest_monster(monsterReqStats);
-		
-    }
-    //move towards whatever target we decided to pick, even if unreachable
-    //(might as well move towards them if no other options)
-    if (!is_in_range(target)){
-      	
-		   
-    } 
-  }
-  
-  moveTowardsTarget(target);
 
+    } else {
+
+      set_message('Hold 4 HP');
+      return; //**these mid-update-function returns are potentially problematic**
+
+    }
+
+  
+  } else { //If we get here, we have a target. 
+	  
+    //This is primarily for melee characters. If they are targeting
+    //something that isn't reachable AND that target isn't already
+    //aggroing, try switching to a closer target.
+    if(!targetIsWithinRestrictedRange(target)&&target.target!==character.name){	
+      target = get_nearest_monster(monsterReqStats);
+    }
+  }
+	
+  if (!is_in_range(target)){
+     moveTowardsTarget(target);
+  } 
+  
   if (can_attack(target)){
       set_message("Attacking");
       attack(target);
@@ -82,8 +89,25 @@ function attackMode(){
 }
 
 function hasEnoughHPForNewTarget(){
+  
 	return (character.hp > minHpRequiredToAcquireNewTarget);
 }
+
+// function getNewTarget(){
+//   game_log("running getNewTarget")
+//   var returnObj = {};
+//   if (get_nearest_monster(monsterReqStats)) {
+//     obj = {
+//       newTarget : get_nearest_monster(monsterReqStats),
+//       message: 'target found'
+//     }
+//   } else {
+//     obj = {
+//       message : 'no monsters'
+//     }
+//   }
+//   return returnObj;
+// }
 
 function targetIsWithinRestrictedRange(target){
   var bool = false;
@@ -124,6 +148,45 @@ function upgradeItemScroll0(itemNameOrLocation){
   );
 	
 }
+
+
+function on_combined_damage(){
+  game_log('TAKEING COMBINED DAMAGE OMAGERRRDDDDDDDDDDDDDDDDDDDDDDD')
+}
+function on_party_invite(name) // called by the inviter's name
+{
+	if (autoAcceptPartyInviteFrom.indexOf(name) >= 0){
+     accept_party_invite(name)
+     game_log('accepted invite from: ' + name);
+  }
+}
+function on_party_request(name) // called by the inviter's name - request = someone requesting to join your existing party
+{
+	if (autoAcceptPartyInviteFrom.indexOf(name) >= 0){
+     accept_party_request(name)
+     game_log('accepted party request from: ' + name);
+  }
+}
+
+function performance_trick()
+{
+	// Needed for browsers only, Steam/Mac versions of the game always deliver high JS performance [03/02/19]
+	parent.performance_trick(); // Just plays an empty sound file, so browsers don't throttle JS, only way to prevent it, interesting cheat [05/07/18]
+	// Lately Chrome has been screwing things up with every update, mostly it's bugs and performance issues, but this time, the way Audio is played has been changed, so, once the game refreshes itself, the tabs need to be manually focused once for performance_trick() to become effective, as Audio can no longer automatically play [21/10/18]
+}
+
+function pm99Bottles(name){
+  for (let i=99; i<0; i--) { 
+    bottles(i); 
+  }
+}
+function bottles(i) { 
+  var message = '';
+  setTimeout(function() { 
+      message = i + ' bottles of beer on the wall! ' + i + ' bottles of beeeeer! Take one down, pass it around, ' + (i - 1) + ' bottles of beer on the wall!';
+      pm(name,message); 
+  }, 8000 * i); 
+} 
 
 //**********  Other global helper vars (supposed to avoid globals oops) ***********//
 
